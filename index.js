3f23f9e372c1858642de3e63bcefa2e3380612ad
isidro lucena santiago <darksirius91@gmail.com>
2020-02-20 12:10:07 +0100

    base v2

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var nick="";
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  nick=prompt("elige nick");
+  console.log(nick +' connected');
   socket.on('chat message', function(msg){
    io.emit('chat message', msg);
   });
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    console.log(nick+' disconnected');
   });
 });
 
